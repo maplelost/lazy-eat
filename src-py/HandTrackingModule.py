0ecca246fa5cbb0a41f6b8235ed850579e8a4a58
徐映白 <62323046+maplelost@users.noreply.github.com>
2025-04-01 14:15:40 +0800

    Update HandTrackingModule.py

--- src-py/HandTrackingModule.py
+++ src-py/HandTrackingModule.py
@@ -198,9 +198,8 @@ class HandDetector:
 
 def main():
     import time
-    cap = cv2.VideoCapture(3, cv2.CAP_DSHOW)
-    # cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))  # 设置编码格式
-    cap.set(cv2.CAP_PROP_FPS, 1)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))  # 设置编码格式
     # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
